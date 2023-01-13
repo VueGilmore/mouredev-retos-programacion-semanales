39ea396681a290923e47f49038a5d43eba87d801
VueGilmore <vuegilmore@gmail.com>
2023-01-13 23:16:18 +0100

    Reto 2022/04 area poligono

---
+++ 2022/reto-04--area-poligono.py
@@ -0,0 +1,24 @@
+'''
+Reto #4: ÁREA DE UN POLÍGONO
+
+Crea una única función (importante que sólo sea una) que sea capaz
+de calcular y retornar el área de un polígono.
+- La función recibirá por parámetro sólo UN polígono a la vez.
+- Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+- Imprime el cálculo del área de un polígono de cada tipo.
+'''
+
+def area_poligono(tipo: str, lados: list):
+  if tipo == 'triangulo':
+    area = lados[0] * lados[1] / 2
+  elif tipo == 'cuadrado':
+    area = lados[0] * lados[0]
+  elif tipo == 'rectangulo':
+    area = lados[0] * lados[1]
+  else:
+      area = 0
+  return area
+
+print(area_poligono('triangulo', [3, 4]))
+print(area_poligono('cuadrado', [5]))
+print(area_poligono('rectangulo', [3, 5]))
