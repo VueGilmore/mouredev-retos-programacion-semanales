660b97b87054a12038d073eb1ceab6cea9a402af
VueGilmore <vuegilmore@gmail.com>
2023-01-13 23:00:51 +0100

    Reto 2022/03 Numeros primos

---
+++ 2022/reto-03--es-numero-primo.py
@@ -0,0 +1,25 @@
+'''
+Reto #3: ¿ES UN NÚMERO PRIMO?
+
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imprime los números primos entre 1 y 100.
+'''
+
+def es_numero_primo(numero):
+    if numero < 2:
+       return False
+
+    for i in range(2, numero):
+      if numero % i == 0:
+        return False
+    
+    return True
+       
+          
+
+def lista_numeros_primos(cantidad):
+    for i in range(1, cantidad + 1):
+      if es_numero_primo(i):
+         print(i)
+
+lista_numeros_primos(100)
